a2236912589454d394fa1d38b552546038c277d9
backiev <farkhat.bakiev@mail.ru>
2026-08-18 21:56:56 +0300

    добавил основную верстку

---
+++ app/components/NewsCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="news-card">
+    <h3 class="news-card__title">Первые земельные участки реализованы на специальных торгах для малого и среднего бизнеса</h3>
+    <div class="news-card__description">На каждый участок претендовали в среднем шесть участников. Стоимость одной из сделок выросла в ходе аукциона в 26 раз.</div>
+    <div class="news-card__link">Подробнее</div>
+    <div class="news-card__bottom">
+      <div class="news-card__original">mos.ru</div>
+      <div class="news-card__date">12.01.2020</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+.news-card {
+  padding: 30px;
+  border-radius: 3px;
+  box-shadow: 
+    0 1px 4px 0 rgba(0, 0, 0, 0.05),
+    0 2px 4px 0 rgba(0, 0, 0, 0.05);
+}
+.news-card__title {
+  font-size: 18px;
+  line-height: 22px;
+  color: var(--main-color);
+}
+.news-card__description {
+  margin-top: 25px;
+  font-size: 14px;
+  line-height: 20px;
+}
+.news-card__link {
+  margin-top: 20px;
+  font-size: 14px;
+  line-height: 20px;
+  text-decoration: underline;
+  color: var(--main-color);
+}
+.news-card__bottom {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.news-card__original,
+.news-card__date {
+  font-size: 14px;
+  line-height: 20px;
+  color: var(--gray-color);
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,72 @@
 <template>
   <div class="index-page global-container">
-    <AppMain />
+    <div class="index-page__header">
+      <h1 class="index-page__title">Список новостей</h1>
+      <div class="index-page__search">Поиск</div>
+    </div>
+    <div class="index-page__controls">
+      <div class="index-page__tabs">
+        <div class="index-page__tab">Все</div>
+        <div class="index-page__tab">Lenta</div>
+      </div>
+      <div class="index-page__view">
+        view
+      </div>
+    </div>
+    <div class="index-page__list">
+      <NewsCard />
+      <NewsCard />
+      <NewsCard />
+      <NewsCard />
+    </div>
+    <div class="index-page__pagination">
+      <div class="index-page__page index-page__page--active">1</div>
+      <div class="index-page__page">2</div>
+      <div class="index-page__page">3</div>
+      <div class="index-page__page">...</div>
+      <div class="index-page__page">200</div>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <style lang="less" scoped>
-
+.index-page__title {
+  font-size: 36px;
+  font-weight: bold;
+}
+.index-page__controls,
+.index-page__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.index-page__controls {
+  margin-top: 63px;
+}
+.index-page__tabs {
+  display: flex;
+  gap: 10px;
+}
+.index-page__list {
+  margin-top: 28px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+}
+.index-page__pagination {
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+.index-page__page {
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 100%;
+}
+.index-page__page--active {
+  color: var(--main-color);
+}
 </style>
